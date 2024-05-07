b855f6dded35aad815372d0e54488cd8946b75f8
duwenjie <duwenjie@stu.scu.edu.cn>
2024-05-08 07:00:18 +0800

    fix: classifier will change the model

--- classifier.py
+++ classifier.py
@@ -10,7 +10,8 @@ from model import Classifier
 
 batch_size = 32
 lr = 0.001
-epochs = 1000
+epochs = 500
+finetune_rate = 0.1
 
 model_file = "output/hhar_pretrain_autoencoder/model.pt"
 
@@ -43,7 +44,7 @@ if __name__ == "__main__":
   utils.set_seeds(2405022300)  # must be the same as the one in train.py
   train_data, train_label, vali_data, vali_label, test_data, test_label = utils.split_data(
       data, label, train_rate=0.8, dev_rate=0.1)
-  train_data, train_label = utils.finetune_data(train_data, train_label, train_rate=0.2)
+  train_data, train_label = utils.finetune_data(train_data, train_label, train_rate=finetune_rate)
   vali_data, vali_label = utils.unique_label(vali_data, vali_label)
   test_data, test_label = utils.unique_label(test_data, test_label)
   
@@ -100,7 +101,6 @@ if __name__ == "__main__":
       if vali_acc > vali_acc_best:
         vali_acc_best = vali_acc
         best_stat = (train_loss, vali_loss, test_loss, train_acc, vali_acc, test_acc, train_f1, vali_f1, test_f1)
-        torch.save(model.state_dict(), model_file)
     
     print('The Total Epoch have been reached.')
     print('Loss:  %0.3f/%0.3f/%0.3f\nBest Accuracy: %0.3f/%0.3f/%0.3f\nF1: %0.3f/%0.3f/%0.3f' % best_stat)
